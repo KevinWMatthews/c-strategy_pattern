379dc38f4007d3a3775757c324d532bfb9fb6897
Kevin Matthews <kmatthews@cyberdata.net>
2016-06-23 17:01:52 -0700

    Refactor

--- src/StrategyPattern.c
+++ src/StrategyPattern.c
@@ -1,16 +1,20 @@
 #include "StrategyPattern.h"
 
+#define BRONZE_RATIO 0.98
+#define SILVER_RATIO 0.95
+#define GOLD_RATIO   0.90
+
 double bronzePriceStrategy(double amount, double shipping)
 {
-    return (0.98 * amount) + shipping;
+    return (BRONZE_RATIO * amount) + shipping;
 }
 
 double silverPriceStrategy(double amount, double shipping)
 {
-    return (0.95 * amount) + shipping;
+    return (SILVER_RATIO * amount) + shipping;
 }
 
 double goldPriceStrategy(double amount, double shipping)
 {
-    return (0.90 * amount);
+    return (GOLD_RATIO * amount);
 }
